2378216fd8f9946bcba85b91aabbb7713c4c80b4
Bucky21659 <mjmiller98@gmail.com>
2018-11-06 18:18:13 -0700

    announce scores are tied when timelimit has been reached

--- code/cgame/cg_playerstate.c
+++ code/cgame/cg_playerstate.c
@@ -434,6 +434,7 @@ void CG_CheckLocalSounds( playerState_t *ps, playerState_t *ops ) {
 		if ( !( cg.timelimitWarnings & 4 ) && msec > ( cgs.timelimit * 60 + 2 ) * 1000 ) {
 			cg.timelimitWarnings |= 1 | 2 | 4;
 			//trap_S_StartLocalSound( cgs.media.suddenDeathSound, CHAN_ANNOUNCER );
+			trap_S_StartLocalSound(cgs.media.tiedLeadSound, CHAN_ANNOUNCER);
 		}
 		else if ( !( cg.timelimitWarnings & 2 ) && msec > (cgs.timelimit - 1) * 60 * 1000 ) {
 			cg.timelimitWarnings |= 1 | 2;
